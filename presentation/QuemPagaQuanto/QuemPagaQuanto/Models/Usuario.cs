8e246991b46e75a5a90fcd860fb5deb20ffefe26
Carlos Júnior <crpu4a@gmail.com>
2024-04-24 18:42:52 -0300

    Criação do projeto e adição de Usuários

---
+++ presentation/QuemPagaQuanto/QuemPagaQuanto/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuemPagaQuanto.Models
+{
+    public enum Perfil
+    {
+        Administrador,
+        Usuario
+    }
+
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        public Perfil Perfil { get; set; }
+    }
+}
