ac9fcb0582f425d9d9a8485c7fbce6e3b916e241
Guilherme Radicchi <guilhermeradicchi@hotmail.com>
2024-04-29 10:27:31 -0300

    Add Despesas

---
+++ presentation/QuemPagaQuanto/QuemPagaQuanto/Migrations/20240429132529_M04-AddTableDespesa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuemPagaQuanto.Migrations
+{
+    public partial class M04AddTableDespesa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Despesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Vencimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Despesas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Despesas");
+        }
+    }
+}
